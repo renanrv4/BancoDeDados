7886796cd2e9467e0c540003a967c017a161e5ec
Diego <dfal@cin.ufpe.br>
2025-07-31 19:03:46 -0300

    Criação de tabelas físicas adaptadas ao novo modelo conceitual

--- projeto_fisico/historico_consultas/create_tables.py
+++ projeto_fisico/historico_consultas/create_tables.py
@@ -11,6 +11,7 @@ CREATE TABLE Grupo(
 );
 """)
 
+#View pra contar o atributo derivado
 #cursor.execute("""
 
 #CREATE VIEW Grupo_View AS
@@ -37,11 +38,44 @@ CREATE TABLE Usuario(
     username VARCHAR(40) NOT NULL,
     First_name VARCHAR(20) NOT NULL,
     Last_name VARCHAR(20) NOT NULL,
-    amigo_ID VARCHAR(10),
-    disc_amigo VARCHAR(8) UNIQUE,
-    CONSTRAINT FK_AMIGO FOREIGN KEY (amigo_ID) REFERENCES Usuario (user_ID)
 );
 """)
+
+#Checar como funcionaria amizade bidirecional
+#cursor.execute("""
+#CREATE TABLE Amizade(
+    
+#            user1_ID VARCHAR(10),
+#            user2_ID VARCHAR(10),
+#            data_inicio DATE NOT NULL,
+#            valido BOOLEAN,
+#          CONSTRAINT Amizade_PK PRIMARY KEY (user1_ID, user2_ID, data_inicio),
+#           CONSTRAINT user1_FK FOREIGN KEY (user1_ID) REFERENCES Usuario (user_ID),
+#           CONSTRAINT user2_FK FOREIGN KEY (user2_ID) REFERENCES Usuario (user_ID)
+#              
+#              );
+
+
+#""")
+
+
+#Opção mais prática: Follow/Unfollow
+
+#cursor.execute("""
+#CREATE TABLE FollowUnfollow(
+    
+#            seguidor_ID VARCHAR(10),
+#            seguido_ID VARCHAR(10),
+#            data_inicio DATE NOT NULL,
+#            valido BOOLEAN,
+#            CONSTRAINT FU_PK PRIMARY KEY (user1_ID, user2_ID, data_inicio),
+#            CONSTRAINT seguidor_FK FOREIGN KEY (user1_ID) REFERENCES Usuario (user_ID),
+#            CONSTRAINT seguido_FK FOREIGN KEY (user2_ID) REFERENCES Usuario (user_ID)
+#              
+#              );
+
+
+#""")
 
 cursor.execute("""
 CREATE TABLE Cartao_credito(
@@ -56,15 +90,27 @@ cursor.execute("""
 CREATE TABLE Jogo(
     jogo_ID VARCHAR(10) PRIMARY KEY,
     preco NUMERIC(5,2) NOT NULL,
-    nome VARCHAR(40) NOT NULL
+    nome VARCHAR(40) NOT NULL,
+    distribuidor_ID VARCHAR(10),
+    desenvolvedor_ID VARCHAR(10),
+               
+    CONSTRAINT DISTRIBUIDOR_FK FOREIGN KEY (distribuidor_ID) REFERENCES Distribuidor (user_ID),
+    CONSTRAINT DESENVOLVEDOR_FK FOREIGN KEY (desenvolvedor_ID) REFERENCES Desenvolvedor (user_ID)
+               
+    
 );
 """)
 
+#Ver se o item fazer parte de um evento é notnull
 cursor.execute("""
 CREATE TABLE Item(
     item_ID VARCHAR(10) PRIMARY KEY,
     nome VARCHAR(20) NOT NULL,
-    decim NUMERIC(7, 5) NOT NULL
+    decim NUMERIC(7, 5) NOT NULL,
+    evento_ID VARCHAR(8), 
+               
+    CONSTRAINT FK_EVENTO FOREIGN KEY (evento_ID) REFERENCES Evento (evento_ID)
+    
 );
 """)
 
@@ -108,30 +154,13 @@ CREATE TABLE Distribuidor(
 
 cursor.execute("""
 CREATE TABLE Desenvolvedor(
-    user_ID VARCHAR(8) PRIMARY KEY,
+    user_ID VARCHAR(10) PRIMARY KEY,
+    desenvolvedor VARCHAR(8) NOT NULL,
     CONSTRAINT FK_USER FOREIGN KEY (user_ID) REFERENCES Usuario (user_ID)
 );
 """)
 
-cursor.execute("""
-CREATE TABLE Desenvolve(
-    developer_ID VARCHAR(8),
-    game_ID VARCHAR(10),
-    CONSTRAINT PK_DEV PRIMARY KEY (developer_ID, game_ID),
-    CONSTRAINT FK_DEV_ID FOREIGN KEY (developer_ID) REFERENCES Desenvolvedor (user_ID),
-    CONSTRAINT FK_GAME FOREIGN KEY (game_ID) REFERENCES Jogo(jogo_ID)
-);
-""")
 
-cursor.execute("""
-CREATE TABLE Distribui(
-    distribuidor_ID VARCHAR(8),
-    game_ID VARCHAR(10),
-    CONSTRAINT PK_DIST PRIMARY KEY (distribuidor_ID, game_ID),
-    CONSTRAINT FK_DIST_ID FOREIGN KEY (distribuidor_ID) REFERENCES Distribuidor (user_ID),
-    CONSTRAINT FK_GAME FOREIGN KEY (game_ID) REFERENCES Jogo(jogo_ID)
-);
-""")
 
 cursor.execute("""
 CREATE TABLE Possui(
@@ -154,5 +183,46 @@ CREATE TABLE UCG(
     CONSTRAINT FK_CARGO FOREIGN KEY (cargo_ID) REFERENCES Cargo (cargo_ID)
 );
 """)
+#Evento - item, Servidor - jogo, Postagem - usuario
+cursor.execute("""
+
+    CREATE TABLE Evento(
+        
+        evento_ID VARCHAR(8) PRIMARY KEY,
+        nome VARCHAR(30),
+        data_inicio DATE
+        );
+
+""")
+
+cursor.execute("""
+    CREATE TABLE Servidor(
+        endereco VARCHAR(10) PRIMARY KEY,
+        ativo BOOLEAN,
+        jogo_ID VARCHAR(10),
+               
+        CONSTRAINT FK_JOGO FOREIGN KEY (jogo_ID) REFERENCES Jogo (jogo_ID)
+               
+        
+        );
+""")
+
+cursor.execute("""
+
+    CREATE TABLE Postagem(
+    user_ID VARCHAR(10),
+    disc_post VARCHAR(12),
+    tipo VARCHAR(10),
+    conteudo VARCHAR(30),
+    
+    CONSTRAINT PK_POST PRIMARY KEY (user_ID, disc_post),
+    CONSTRAINT FK_POST FOREIGN KEY (user_id) REFERENCES Usuario (user_ID)
+    );
+
+
+
+""")
+
+
 
 connection.close()
