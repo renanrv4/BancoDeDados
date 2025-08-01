38b0a44737c2e97a2e57af4245d688643761c75a
hugoniceas <niceasoriginal@gmail.com>
2025-07-31 23:08:59 -0300

    removidos identificadores artificiais do caso 3 do mongoDB servidor/jogo

--- projeto_fisico/mongo_hugo/case3.py
+++ projeto_fisico/mongo_hugo/case3.py
@@ -13,10 +13,10 @@ collection_jogo.delete_many({})
 collection_servidor.delete_many({})
 
 # Mapeamento do caso 3 (um documento com um array de referências para documentos) para o relacionamento Jogo/Servidor
-# Como aqui os servidores são um documento de ordem maior, o mongo associa a eles um id, porém ainda colocaremos um identificador artificial
-lista_servidores_cs2 = [{'servidor_ID': 1,'ativo' : True},
-                    {'servidor_ID': 2,'ativo' : False},
-                    {'servidor_ID': 3,'ativo' : True}]
+# Como aqui os servidores são um documento de ordem maior, o mongo associa a eles um id
+lista_servidores_cs2 = [{'ativo' : True},
+                    {'ativo' : False},
+                    {'ativo' : True}]
 
 lista_servidores_cs2_insert = collection_servidor.insert_many(lista_servidores_cs2)
 servidores_cs2_id = lista_servidores_cs2_insert.inserted_ids
@@ -25,8 +25,8 @@ jogo_cs2 = {'nome': 'CS2',
             'preço': 86,
             'servidores': servidores_cs2_id}
 
-lista_servidores_paladins =  [{'servidor_ID': 4,'ativo' : True},
-                            {'servidor_ID': 5,'ativo' : True}]
+lista_servidores_paladins =  [{'ativo' : True},
+                            {'ativo' : True}]
 
 lista_servidores_paladins_insert = collection_servidor.insert_many(lista_servidores_paladins)
 servidores_paladins_id = lista_servidores_paladins_insert.inserted_ids
@@ -35,11 +35,11 @@ jogo_paladins = {'nome' : 'Paladins',
                 'preço': 0,
                 'servidores': servidores_paladins_id}
 
-lista_servidores_dbd = [{'servidor_ID': 6,'ativo' : True},
-                    {'servidor_ID': 7,'ativo' : True},
-                    {'servidor_ID': 8,'ativo' : False},
-                    {'servidor_ID': 9,'ativo' : False},
-                    {'servidor_ID': 10,'ativo' : False}]
+lista_servidores_dbd = [{'ativo' : True},
+                    {'ativo' : True},
+                    {'ativo' : False},
+                    {'ativo' : False},
+                    {'ativo' : False}]
 
 lista_servidores_dbd_insert = collection_servidor.insert_many(lista_servidores_dbd)
 servidores_dbd_id = lista_servidores_dbd_insert.inserted_ids
@@ -58,4 +58,4 @@ id_servidores = documento_dbd['servidores']
 
 for id in id_servidores:
     documento_servidor = collection_servidor.find_one({'_id' : id})
-    print(f"{documento_servidor['servidor_ID']} : está ativo? -> {documento_servidor['ativo']}")
+    print(f"{id} : está ativo? -> {documento_servidor['ativo']}")
