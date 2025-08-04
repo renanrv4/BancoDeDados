e80dadb09f59ce65c3a530e4dfab5b5c691503a6
luis-erf <luisedu2395@gmail.com>
2025-08-04 18:36:53 -0300

    Adicionando o comentário na operação de conjunto

--- projeto_fisico/historico_consultas/op_de_conjunto.py
+++ projeto_fisico/historico_consultas/op_de_conjunto.py
@@ -3,6 +3,8 @@ import sqlite3
 connection = sqlite3.connect('steam.db')
 cursor = connection.cursor()
 
+#Retorna os identificadores dos usuários que são desenvolvedores e distribuidores
+
 cursor.execute("""
     SELECT user_ID FROM Desenvolvedor
     INTERSECT
