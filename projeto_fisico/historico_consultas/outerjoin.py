5fd68d7ae1390eea7c54acbf702144de46dc9e55
luis-erf <luisedu2395@gmail.com>
2025-08-05 09:30:18 -0300

    Update outerjoin.py

--- projeto_fisico/historico_consultas/outerjoin.py
+++ projeto_fisico/historico_consultas/outerjoin.py
@@ -5,7 +5,7 @@ cursor = connection.cursor()
 
 # Retorna todos os usuários que nunca fizeram uma compra
 
-cursor.execute("SELECT U.username FROM Usuario U LEFT JOIN COMPRA C ON U.user_ID = C.user_ID WHERE C.user_ID IS NULL AND U.user_ID NOT IN (SELECT user_ID FROM Distribuidor) AND U.user_ID NOT IN (SELECT user_ID FROM Desenvolvedor)")
+cursor.execute("SELECT U.username FROM Usuario U LEFT JOIN COMPRA C ON U.user_ID = C.user_ID WHERE C.user_ID IS NULL")
 
 for i in cursor.fetchall():
     print(i)
